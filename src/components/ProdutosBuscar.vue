8a3c21add39c9219581a772960d0dc83bc3531b1
caique camargo <caique.de.camargo@hotmail.com>
2020-06-16 15:16:01 -0300

    Working in products list

---
+++ src/components/ProdutosBuscar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Produtos Buscar</div>
+</template>
+
+<script>
+export default {
+  name: "ProdutosBuscar"
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,30 @@
 <template>
-  <div class="home"></div>
+  <section>
+    <h1>Compre ou Venda</h1>
+    <ProdutosBuscar />
+    <ProdutosLista />
+  </section>
 </template>
 
 <script>
+import ProdutosBuscar from "@/components/ProdutosBuscar.vue";
+import ProdutosLista from "@/components/ProdutosLista.vue";
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    ProdutosBuscar,
+    ProdutosLista
+  }
 };
 </script>
+
+<style lang="scss" scoped>
+h1 {
+  @include font;
+  font-size: 2rem;
+  margin-top: 40px;
+  color: $second_color;
+  text-align: center;
+}
+</style>
